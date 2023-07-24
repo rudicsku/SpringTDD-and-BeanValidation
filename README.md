4c90c5382fa23f75e15a6364c0966aab1b9a69ae
HU900849 <Rudolf.Nemeth@tesco.com>
2023-07-24 14:38:19 +0200

    extended README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SpringTDD-and-BeanValidation
+
+Validation in SpringBoot: https://www.baeldung.com/spring-boot-bean-validation
+Spring boot testing: https://www.baeldung.com/spring-boot-testing
+
+
--- src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
+++ src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
@@ -4,9 +4,12 @@ package com.ltp.gradesubmission;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -14,71 +17,72 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
- - Integration test maps the entire request and  response lifecycle, verifies the interactions of the application's layers
- - We need the Spring Context when integration testing, hte test needs access to the entire Spring Container
- - Unit tests comes first
+ * - Integration test maps the entire request and  response lifecycle, verifies the interactions of the application's layers
+ * - We need the Spring Context when integration testing, the test needs access to the entire Spring Container
+ * - Unit tests comes first
  */
 
 @SpringBootTest
 @AutoConfigureMockMvc
+//@TestPropertySource(
+//	We can override application.properties with test specific properties
+//)
 class GradeSubmissionApplicationTests {
 
 
-	@Autowired
-	//We need MockMvc to mock GET or POST request against controller
-	private MockMvc mockMvc;
+    @Autowired
+    //We need MockMvc to mock GET or POST request against controller
+    private MockMvc mockMvc;
 
-	@Test
-	void contextLoads() {
-		assertNotNull(mockMvc);
-	}
+    @Test
+    void contextLoads() {
+        assertNotNull(mockMvc);
+    }
 
-	@Test
-	void testShowGradeForm() throws Exception {
-		RequestBuilder request = MockMvcRequestBuilders.get("/?id=123");
+    @Test
+    void testShowGradeForm() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/?id=123");
 
-		mockMvc.perform(request)
-			.andExpect(status().is2xxSuccessful())
-			.andExpect(view().name("form"))
-			.andExpect(model().attributeExists("grade"));
-	}
+        mockMvc.perform(request)
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("form"))
+                .andExpect(model().attributeExists("grade"));
+    }
 
 
-	@Test
-	void testSuccessfulSubmission() throws Exception {
+    @Test
+    void testSuccessfulSubmission() throws Exception {
 
-		RequestBuilder request = MockMvcRequestBuilders.post("/handleSubmit")
-			.param("name", "Harry")
-			.param("subject", "Potions")
-			.param("score", "C-");
+        RequestBuilder request = MockMvcRequestBuilders.post("/handleSubmit")
+                .param("name", "Harry")
+                .param("subject", "Potions")
+                .param("score", "C-");
 
-			mockMvc.perform(request)
-				.andExpect(status().is3xxRedirection())
-				.andExpect(redirectedUrl("/grades"));
-	}
+        mockMvc.perform(request)
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/grades"));
+    }
 
-	@Test
-	void testUnsuccessfulSubmission() throws Exception {
-		RequestBuilder request = MockMvcRequestBuilders.post("/handleSubmit")
-			.param("name", "    ")
-			.param("subject", "     ")
-			.param("score", "R+");
+    @Test
+    void testUnsuccessfulSubmission() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.post("/handleSubmit")
+                .param("name", "    ")
+                .param("subject", "     ")
+                .param("score", "R+");
 
-			mockMvc.perform(request)
-				.andExpect(status().is2xxSuccessful())
-				.andExpect(view().name("form"));
-	}
+        mockMvc.perform(request)
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("form"));
+    }
 
-	@Test
-	void testGetGrades() throws Exception {
-		RequestBuilder request = MockMvcRequestBuilders.get("/grades");
-		mockMvc.perform(request)
-			.andExpect(status().is2xxSuccessful())
-			.andExpect(view().name("grades"))
-			.andExpect(model().attributeExists("grades"));
-	}
-	
-
+    @Test
+    void testGetGrades() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/grades");
+        mockMvc.perform(request)
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(view().name("grades"))
+                .andExpect(model().attributeExists("grades"));
+    }
 
 
 }
